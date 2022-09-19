5fbaba933f400940939774f38f775b363d5b5da0
Rusu Cristian 1E3 <39808975+Plutonium8T8@users.noreply.github.com>
2022-09-19 01:40:25 -0700

    v0.1.1
    repo interface

---
+++ CarApp/Entity/Repository/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Repository.Interfaces
+{
+    internal interface IRepository<T> : IDisposable
+    {
+        IEnumerable<T> GetEntityList();
+        Task<T> GetEntity(long id);
+        Task Create(T item);
+        Task Update(T item);
+        Task Delete(T item);
+        Task Save();
+    }
+}
