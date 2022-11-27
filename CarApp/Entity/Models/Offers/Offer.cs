c9668c9e0dd08c6ff40ed52c1cf1059e27f12c44
Rusu Cristian 1E3 <39808975+Plutonium8T8@users.noreply.github.com>
2022-11-27 10:39:55 -0800

    offer update

--- CarApp/Entity/Models/Offers/Offer.cs
+++ CarApp/Entity/Models/Offers/Offer.cs
@@ -11,10 +11,14 @@ namespace Entity.Models.Offers
 {
     public class Offer : EntityBaseClass
     {
-        public long Id { get; set; }
         public string Title { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Price { get; set; }
+        public string Location { get; set; }
+        public string Engine { get; set; }
         public string Description { get; set; }
-        public Car ImpliedCar { get; set; }
+        public string Extra { get; set; }
         public virtual long UserId { get; set; }
         public virtual User User { get; set; }
     }
